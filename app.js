d773c45f26ad8fb12fec3b3f54ccd25094f60ddd
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2024-06-09 23:56:55 +1000

    edit

---
+++ app.js
@@ -0,0 +1,41 @@
+const image = document.querySelector('.image');
+
+window.addEventListener('scroll', () => {
+    // Calculate blur based on scroll position
+    const scrollPosition = window.scrollY;
+    const blurAmount = (scrollPosition / 80); // Adjust as needed
+
+    // Apply the blur effect with smoother transition
+    image.style.filter = `blur(${0.5}px)`;
+});
+
+const textOverlay = document.querySelector('.text-overlay');
+const imageSection = document.querySelector('.image-section');
+
+// Function to handle scroll events and show the text and logo
+function handleScroll() {
+    // Once the user scrolls, immediately reveal the text and logo
+    textOverlay.style.opacity = 1;
+    
+    // Remove the scroll event listener to prevent further changes
+    window.removeEventListener('scroll', handleScroll);
+}
+
+// Listen for the first scroll event and call the handleScroll function
+window.addEventListener('scroll', handleScroll);
+
+
+
+var scrolling = document.querySelector('.scrolling');
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    if (window.scrollY >150){
+        scrolling.classList.add('active');
+
+    }
+
+    else{
+        scrolling.classList.remove('active')
+    }
+}
